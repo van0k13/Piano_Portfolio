3ebbbe856d70482472ade01f874fd3cd3af85fed
van0k13 <kazak13v@gmail.com>
2020-03-23 21:42:33 +0200

    done homevideo block with grid

--- src/BLL/HomeVideoReducer.js
+++ src/BLL/HomeVideoReducer.js
@@ -1,18 +1,25 @@
-
+import proj1 from '../assets/img/proj1.jpg'
+import proj2 from '../assets/img/proj2.jpg'
+import proj3 from '../assets/img/proj3.jpg'
+import proj4 from '../assets/img/proj4.jpg'
+import proj5 from '../assets/img/proj5.jpg'
+import proj6 from '../assets/img/proj6.jfif'
+import proj7 from '../assets/img/proj7.jpg'
+import proj8 from '../assets/img/proj8.jpg'
+import proj9 from '../assets/img/proj.jpg'
 
 
 
 const initialState = [
-    {id: 1, name: 'Project1', type: 'Video'},
-    {id: 2, name: 'Project2', type: 'Video'},
-    {id: 3, name: 'Project2', type: 'Video'},
-    {id: 4, name: 'Project2', type: 'Video'},
-    {id: 5, name: 'Project3', type: 'Video'},
-    {id: 6, name: 'Project4', type: 'Photo'},
-    {id: 7, name: 'Project5', type: 'Photo'},
-    {id: 8, name: 'Project5', type: 'Photo'},
-    {id: 9, name: 'Project5', type: 'Photo'},
-    {id: 10, name: 'Project6', type: 'Photo'}
+    {id: 1, name: 'Project1', img: proj1, type: 'Video'},
+    {id: 2, name: 'Project2', img: proj2, type: 'Video'},
+    {id: 3, name: 'Project3', img: proj3, type: 'Video'},
+    {id: 4, name: 'Project4', img: proj4, type: 'Video'},
+    {id: 5, name: 'Project5', img: proj5, type: 'Video'},
+    {id: 6, name: 'Project6', img: proj6, type: 'Photo'},
+    {id: 7, name: 'Project7', img: proj7, type: 'Photo'},
+    {id: 8, name: 'Project8', img: proj8, type: 'Photo'},
+    {id: 9, name: 'Project9', img: proj9, type: 'Photo'}
 ]
 
 
--- src/Components/HomeVideo/Body/HomeVideoBody.js
+++ src/Components/HomeVideo/Body/HomeVideoBody.js
@@ -2,13 +2,18 @@ import React from 'react';
 import styles from './HomeVideoBody.module.css'
 
 const HomeVideoBody = (props) => {
-  const projectsElements = props.projects.map(p =>  <div key={p.id} className={styles.project}>{p.name}</div>)
+  const projectsElements = props.projects.map(p =>
+    <div key={p.id} className={styles.project}>
+      <img src={p.img} />
+      
+    </div>
+  )
   return (
     <div className={styles.bodyContainer}>
       <div className={styles.buttonsContainer}>
-        <button className={styles.button} onClick={()=>props.setFilterValue('All')}>All</button>
-        <button className={styles.button} onClick={()=>props.setFilterValue('Photo')}>Photos</button>
-        <button className={styles.button} onClick={()=>props.setFilterValue('Video')}>Videos</button>
+        <button className={styles.button} onClick={() => props.setFilterValue('All')}>All</button>
+        <button className={styles.button} onClick={() => props.setFilterValue('Photo')}>Photos</button>
+        <button className={styles.button} onClick={() => props.setFilterValue('Video')}>Videos</button>
       </div>
       <div className={styles.projectsContainer}>
         {projectsElements}
