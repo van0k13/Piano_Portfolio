bcfb0afb0eeef95c245ac95fdf1777973639e78e
van0k13 <kazak13v@gmail.com>
2021-01-25 21:13:43 +0200

    initial state type creating

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MainApp from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
