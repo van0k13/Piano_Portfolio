e3c22e143f8ea7c256ca2731094f982edc1e4b48
van0k13 <kazak13v@gmail.com>
2021-02-23 23:21:21 +0200

    added images. Changed image displaying. Added enlarging image

--- src/Components/MainComponent/HomeVideo/Body/HomeVideoBody.tsx
+++ src/Components/MainComponent/HomeVideo/Body/HomeVideoBody.tsx
@@ -1,7 +1,13 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
+import Zoom from '@material-ui/core/Zoom';
 import { Project } from '../../../../types';
 import { ButtonWithHookSupp } from '../../../SupportiveComponents/ButtonComponent/Button';
-import styles from './HomeVideoBody.module.scss'
+import { GridListTileBar, Paper } from '@material-ui/core';
 
 interface iProps {
   projects: Project[],
@@ -9,26 +15,90 @@ interface iProps {
   buttonNames: string[]
 }
 
-const HomeVideoBody: React.FC<iProps> = ({setFilterValue, projects, buttonNames}) => {
-  const projectsElements = projects.map((p) => 
-    <div key={p.id} className={styles.project}>
-      <img src={p.img} alt='ptoject'/>
-      
-    </div>
-  )
+const useStyles = makeStyles({
+  wrapper: {
+    position: 'relative',
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  container: {
+    position: 'fixed',
+    left: 0,
+    right: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 100,
+    width: 'auto',
+    height: '100vh'
+  },
+  jpg: {
+    width: 'auto',
+    height: '100vh'
+  },
+  root: {
+    maxWidth: 345,
+    margin: "1rem",
+    transition: "transform 1s ease-in-out"
+  },
+  media: {
+    height: 540,
+    width: 500,
+    '&:hover': {
+      transform: "scale3d(1.05, 1.05, 1)",
+    }
+  },
+});
+
+const HomeVideoBody: React.FC<iProps> = ({ setFilterValue, projects, buttonNames }) => {
+  const [currentImageId, setCurrentImageId] = useState<number>();
+  const [showEnlargedImage, setShowEnlargedImage] = useState<boolean>(false);
+
+  const classes = useStyles();
+
+  const projectsElements = projects.map((p, i) =>
+    <Card className={classes.root} key={i}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={p.img}
+          onClick={() => onImageClickHandler(p.id)}
+        />
+        <GridListTileBar
+          title={p.name}
+        />
+      </CardActionArea>
+      <CardActions>
+
+      </CardActions>
+    </Card>
+  );
+
+  const onImageClickHandler = (id: number) => {
+    setCurrentImageId(id)
+    setShowEnlargedImage(true)
+  }
+
   const buttonElements = buttonNames.map(b => <ButtonWithHookSupp buttonName={b}
-   buttonOnClick={setFilterValue} buttonValue={b} noContainer />)
+    buttonOnClick={setFilterValue} buttonValue={b} noContainer />);
+
+  const foundElement = projects.find(el => el.id === currentImageId);
+  
   return (
-    <div className={styles.bodyContainer}>
-      <div className={styles.buttonsContainer}>
-        {buttonElements}
-      </div>
-      <div className={styles.projectsContainer}>
-        {projectsElements}
-      </div>
+    <div className={classes.wrapper}>
+      {projectsElements}
+      {showEnlargedImage &&
+        <div className={classes.container}>
+          <Zoom in={showEnlargedImage} >
+            <Paper variant="outlined" onClick={() => setShowEnlargedImage(false)} onBlur={() => setShowEnlargedImage(false)}>
+              <img className={classes.jpg} src={foundElement?.img} />
+            </Paper>
+          </Zoom>
+        </div>
+      }
     </div>
 
   );
-}
+};
 
 export default HomeVideoBody;
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,13 @@
-import proj1 from './assets/img/girlWithLeaf.jpg'
-import proj2 from './assets/img/hands.jpg'
-import proj3 from './assets/img/Koti.jpg'
-import proj4 from './assets/img/noti.jpg'
-import proj5 from './assets/img/piano.png'
-import proj6 from './assets/img/piano1.jpg'
 import proj7 from './assets/img/piano2.jpg'
+import proj8 from './assets/img/IMG_1238.jpg'
+import proj9 from './assets/img/IMG_1452.jpg'
+import proj10 from './assets/img/IMG_3215.jpg'
+import proj11 from './assets/img/IMG_4420.jpg'
+import proj12 from './assets/img/IMG_5452.jpg'
+import proj13 from './assets/img/IMG_5476.jpg'
+import proj14 from './assets/img/IMG_7889.jpg'
+import proj15 from './assets/img/IMG_8087.jpg'
+import proj16 from './assets/img/IMG_9253.jpg'
 import introImage from './assets/img/piano3.jpg'
 import aboutMeImage from './assets/img/Koti.jpg'
 import sloganImage from './assets/img/noti.jpg'
@@ -17,13 +20,16 @@ export const initialState: IInitialState = {
     homeVideoComponent: 
     {
         projects: [
-            {id: 1, name: 'Project1', img: proj1, type: 'Video'},
-            {id: 2, name: 'Project2', img: proj2, type: 'Video'},
-            {id: 3, name: 'Project3', img: proj3, type: 'Video'},
-            {id: 4, name: 'Project4', img: proj4, type: 'Video'},
-            {id: 5, name: 'Project5', img: proj5, type: 'Video'},
-            {id: 6, name: 'Project6', img: proj6, type: 'Photo'},
-            {id: 7, name: 'Project7', img: proj7, type: 'Photo'}
+            {id: 7, name: 'Project7', img: proj7, type: 'Photo'},
+            {id: 8, name: 'Project8', img: proj8, type: 'Photo'},
+            {id: 9, name: 'Project9', img: proj9, type: 'Photo'},
+            {id: 10, name: 'Project10', img: proj10, type: 'Photo'},
+            {id: 11, name: 'Project11', img: proj11, type: 'Photo'},
+            {id: 12, name: 'Project12', img: proj12, type: 'Photo'},
+            {id: 13, name: 'Project13', img: proj13, type: 'Photo'},
+            {id: 14, name: 'Project14', img: proj14, type: 'Photo'},
+            {id: 15, name: 'Project15', img: proj15, type: 'Photo'},
+            {id: 16, name: 'Project16', img: proj16, type: 'Photo'}
         ],
         headerTextH1Value: 'My',
         headerTextSpanValue: 'Performance',
